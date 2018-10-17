a77236c6a664711a8bd0cad807b607f73db1cdd0
Jeremy Beck <jeremycbeck@outlook.com>
2018-10-17 09:23:33 -0700

    initial setup

---
+++ lab07-collections/lab07-collections/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lab07_collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
